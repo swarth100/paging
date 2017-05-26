f7f9dc8a3a9a9e165b545fe59f52648ec0ee48e4
Alberto Spina <as12015@ic.ac.uk>
2017-05-26 22:07:26 +0100

    index.js: Add saving example to index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 console.log("Hello");
 
-var mongooseEntry = require('./server/mongoose/entry');
 var mongooseUser = require('./server/mongoose/user');
+/* var mongooseEntry = require('./server/mongoose/entry'); */
 
-var user = mongooseUser.createNewUser('Bob', 'EXAMPLE@example.com', '1234');
+var user = mongooseUser.createNewUser('Anne', 'EXAMPLE@example.com', '98765');
 
 console.log(user.debugPrinting());
 console.log(user.name);
 console.log(user.email);
+
+user.saveUser();
