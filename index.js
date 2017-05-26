a128c628d90a2569a9c20d92338f8c2d71aede90
Alberto Spina <as12015@ic.ac.uk>
2017-05-26 23:37:53 +0100

    index.js: Fix testing for new usernames

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ console.log("Hello");
 var mongooseUser = require('./server/mongoose/user');
 var mongooseEntry = require('./server/mongoose/entry');
 
-var user = mongooseUser.createNewUser('Anne 10', 'EXAMPLE@example.com', '12345', 'admin 4');
+var user = mongooseUser.createNewUser('Anne 10', 'EXAMPLE@example.com', '12345', 'admin 10000000');
 
 console.log(user.debugPrinting());
 console.log(user.name);
