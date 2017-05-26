d74bdc91ac4662efea878a65b048ba110ef302e3
Alberto Spina <as12015@ic.ac.uk>
2017-05-26 22:59:21 +0100

    index.js: Add multiple concurrent connections

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 console.log("Hello");
 
 var mongooseUser = require('./server/mongoose/user');
-/* var mongooseEntry = require('./server/mongoose/entry'); */
+var mongooseEntry = require('./server/mongoose/entry');
 
-var user = mongooseUser.createNewUser('Anne', 'EXAMPLE@example.com', '98765');
+var user = mongooseUser.createNewUser('Anne 10', 'EXAMPLE@example.com', '12345', 'admin 4');
 
 console.log(user.debugPrinting());
 console.log(user.name);
 console.log(user.email);
 
 user.saveUser();
+
+var user2 = mongooseUser.createNewUser('Anne 4', 'EXAMPLE@example.com', '12345', 'admin');
+user2.saveUser();
