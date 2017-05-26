d19f1a337d73cda0f72de448fa6b348b0ad63215
Alberto Spina <as12015@ic.ac.uk>
2017-05-26 20:39:09 +0100

    server/mongoose/entry.js: Rename from index.js

--- server/mongoose/entry.js
+++ server/mongoose/entry.js
@@ -2,33 +2,34 @@ var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://cloud-vm-45-124.doc.ic.ac.uk:27017');
 
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
 var Schema = mongoose.Schema;
 
+// Initialise the exported modules
 var exports = module.exports = {};
 
 var entrySchema = new Schema({
     name: String,
     city: String,
-    updated: { type: Date, default: Date.now },
+    // updated: { type: Date, default: Date.now },
     coordinates : { latitude: Number, longitude: Number}
 });
 
 // Helper methods on entrySchema
-entrySchema.methods.doSomething = function() {
-
-    return this.name;
-}
+entrySchema.methods.debugPrinting = function() {
+    return 'name: ' + this.name + ', city: ' + this.city;
+};
 
 
 
 var Entry = mongoose.model('Entry', entrySchema);
 
 exports.createNewEntry = function (a, b) {
-    var newEntry = new Entry({
+    return new Entry({
         name : a,
         city : b
     });
-
-    return newEntry;
 };
 
