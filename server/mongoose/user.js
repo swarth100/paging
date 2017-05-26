4f5e3ad3980c10a43af39f833312a8ff1642b631
Alberto Spina <as12015@ic.ac.uk>
2017-05-26 22:07:06 +0100

    server/mongoose/user.js: Implement dabatabse saving. Can now hold multiple connections

--- server/mongoose/user.js
+++ server/mongoose/user.js
@@ -1,18 +1,23 @@
 var mongoose = require('mongoose');
 
 /* Connect to mongoDB entry database */
-var userDB = '/users';
-mongoose.createConnection('mongodb://cloud-vm-45-124.doc.ic.ac.uk:27017' + userDB);
+
+var Schema = mongoose.Schema;
+var userDBName = '/users';
+mongoose.Promise = global.Promise;
+var userDB = mongoose.createConnection('mongodb://cloud-vm-45-124.doc.ic.ac.uk:27017' + userDBName);
 
 // TODO: Add validation
 
 /* Handling connection errors */
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
 
-var Schema = mongoose.Schema;
+userDB.on('error', console.error.bind(console, 'connection error:'));
+userDB.once('open', function() {
+    console.log('User DB Used');
+});
 
 /* Initialise the exported modules */
+
 var exports = module.exports = {};
 
 /* Setters for the fields */
@@ -22,9 +27,9 @@ function toLower (v) {
 }
 
 var userSchema = new Schema({
-    name: String,
+    name: { type: String, required: true },
     email: {type: String, set: toLower},
-    password: String
+    password: { type: String, required: true }
 });
 
 /* Helper methods on the userSchema */
@@ -33,13 +38,24 @@ var userSchema = new Schema({
 * Parameters:
 *   none
 * Returns:
-*   none*/
+*   none */
 userSchema.methods.debugPrinting = function() {
     return 'name: ' + this.name + ', email: ' + this.email + ', password: ' + this.password;
 };
 
+/* Saves the current User onto the DB
+ * Parameters:
+ *   none
+ * Returns:
+ *   none */
+userSchema.methods.saveUser = function () {
+    this.save(function (err) {
+        if (err) return handleError(err);
+        // Saved!
+    });
+}
 
-var User = mongoose.model('User', userSchema);
+var User = userDB.model('User', userSchema);
 
 /* Creates and returns a new database entry
 * Parameters:
@@ -47,7 +63,7 @@ var User = mongoose.model('User', userSchema);
 *   e = email
 *   p = password
 * Returns:
-*   new User instance*/
+*   new User instance */
 exports.createNewUser = function (n, e, p) {
     return new User({
         name : n,
@@ -56,7 +72,5 @@ exports.createNewUser = function (n, e, p) {
     });
 };
 
-/* Saves a new database entry */
-
-// TODO
+exports.userModel = User;
 
