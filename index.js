1a612b1b4a9a1ebb1d35035730fb996a10c8daa6
Alberto Spina <as12015@ic.ac.uk>
2017-05-27 14:38:07 +0100

    index.js: Fix index.js to work with newly introduced syntax

--- index.js
+++ index.js
@@ -1,24 +1,13 @@
 var mongooseUser = require('./server/mongoose/user');
 var mongooseEntry = require('./server/mongoose/entry');
 
-var user = mongooseUser.createNewUser('Anne', 'EXAMPLE@example.com', '12345', 'unique 7');
+var user = mongooseUser.createNewUser('Anne', 'EXAMPLE@example.com', '123456', 'anne_1');
 
-var savePromise = user.saveUser();
+var savePromise = mongooseUser.saveUser(user);
 savePromise
     .then(function (user) {
         console.log(user.debugPrinting());
-})
-    .catch(function (err) {
-        console.log('Error occurred while calling save');
-});
-
-var findPromise  = user.findMultiple({name : 'Anne', email: 'example@example.com'});
-findPromise
-    .then(function (users) {
-        for (var i = 0; i < users.length; i++) {
-            console.log(users[i].debugPrinting());
-        }
     })
     .catch(function (err) {
-        console.log('No element in the database meets the search criteria');
+        console.log('Error occurred while calling save');
     });
