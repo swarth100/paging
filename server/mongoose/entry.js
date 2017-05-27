31bf997af1b23729de1944c1058ceb13f14a9703
Alberto Spina <as12015@ic.ac.uk>
2017-05-27 16:46:44 +0100

    server/mongoose/entry.js: Add location DB operations

--- server/mongoose/entry.js
+++ server/mongoose/entry.js
@@ -1,25 +1,31 @@
+// TODO: Add description
+
+/* ------------------------------------------------------------------------------------------------------------------ */
+
 var mongoose = require('mongoose');
+var helper = require('./mongoose');
+
+var uniqueValidator = require('mongoose-unique-validator');
 
 /* Connect to mongoDB entry database */
 
 var Schema = mongoose.Schema;
-var entryDBName = '/entries';
+var locationsDBName = '/locations';
 mongoose.Promise = global.Promise;
-var entryDB = mongoose.createConnection('mongodb://cloud-vm-45-124.doc.ic.ac.uk:27017' + entryDBName);
+var locationsDB = mongoose.createConnection('mongodb://cloud-vm-45-124.doc.ic.ac.uk:27017' + locationsDBName);
 
 // TODO: Add validation
 
 /* Handling connection errors */
-var db = mongoose.connection;
-entryDB.on('error', console.error.bind(console, 'Cannot connect to entryDB:'));
-entryDB.once('open', function() {
+locationsDB.on('error', console.error.bind(console, 'Cannot connect to entryDB:'));
+locationsDB.once('open', function() {
     console.log('Entry DB Active');
 });
 
 // Initialise the exported modules
 var exports = module.exports = {};
 
-var entrySchema = new Schema({
+var locationsSchema = new Schema({
     id : {
         type : Number,
         unique : true,
@@ -44,20 +50,92 @@ var entrySchema = new Schema({
     }
 });
 
+/* Plugin that validates unique entries */
+locationsSchema.plugin(uniqueValidator);
+
 /* Helper methods on entrySchema */
-entrySchema.methods.debugPrinting = function() {
-    return 'name: ' + this.name + ', city: ' + this.city;
+locationsSchema.methods.debugPrinting = function() {
+    return 'id: ' + this.id + ', latitude: ' + this.latitude + ', longitude: ' + this.longitude;
 };
 
-var Entry = entryDB.model('Entry', entrySchema);
+/* Pre save function [AUTORUN]
+ * Used to initialise fields upon saving
+ * */
+locationsSchema.pre('save', function(next) {
+    this.time = Date.now();
 
-/* Creates and returns a new database entry */
-exports.createNewEntry = function (a, b) {
-    return new Entry({
-        name : a,
-        city : b
+    // TODO: Handle checks before invoking next
+    // Next can be invoked with an error to make it cascade through
+    // i.e. new Error('something went wrong')
+    next();
+});
+
+/* ------------------------------------------------------------------------------------------------------------------ */
+
+var Location = locationsDB.model('Location', locationsSchema);
+
+/* Creates and returns a new database entry
+ * Parameters:
+ *   a = id
+ *   b = avgtime
+ *   c = latitude
+ *   d = longitude
+ * Returns:
+ *   new Location instance */
+exports.createNewLocation = function (a, b, c, d) {
+    return new Location({
+        id : a,
+        avgtime : b,
+        latitude : c,
+        longitude : d
     });
 };
 
-/* Saves a new database entry */
+/* Saves the current Location onto the DB
+ * Parameters:
+ *   user
+ * Returns:
+ *   Promise */
+exports.saveLocation = function (loc) {
+    return helper.saveHelper(loc);
+};
+
+/* Retrieves one Location from the DB
+ * Parameters:
+ *   Search parameters : { id : 34 }
+ * Returns:
+ *   Promise */
+exports.find = function (p) {
+    return helper.findHelper(Location, p);
+};
+
+/* Retrieves multiple Locations from the DB
+ * Parameters:
+ *   Search parameters : { id : 34 }
+ * Returns:
+ *   Promise */
+exports.findMultiple = function (p) {
+    return helper.findMultipleHelper(Location, p);
+};
+
+/* Removes a single Location from the DB
+ * Parameters:
+ *   Search parameters : { id : 34 }
+ * Returns:
+ *   Promise */
+exports.removeUser = function (p) {
+    return helper.removeElem(Location, p);
+};
+
+/* Removes multiple Locations from the DB
+ * Parameters:
+ *   Search parameters : { id : 34 }
+ * Returns:
+ *   Promise */
+exports.removeMultiple = function (p) {
+    return helper.removeMultipleHelper(Location, p);
+};
+
+/* Export the User model */
+exports.locationModel = Location;
 
