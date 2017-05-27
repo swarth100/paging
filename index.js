a06fb3e2c50125ec2b731b47ca6a05273552f63c
Alberto Spina <as12015@ic.ac.uk>
2017-05-27 17:16:08 +0100

    index.js: Add location debugging

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
-var mongooseUser = require('./server/mongoose/user');
-var mongooseEntry = require('./server/mongoose/entry');
+var mongooseLocation = require('./server/mongoose/location');
 
-var user = mongooseUser.createNewUser('Anne', 'EXAMPLE@example.com', '123456', 'anne_1');
+/* Create a new location */
+var location = mongooseLocation.createNewLocation(2, 200, 100, 100);
 
-var savePromise = mongooseUser.saveUser(user);
+/* Print the fields of the newly created location */
+console.log(location.id);
+console.log(location.latitude);
+console.log(location.longitude);
+
+/* Apply functions to the newly created location */
+console.log(location.debugPrinting());
+
+/* Save the location to the database */
+var savePromise = mongooseLocation.saveLocation(location);
 savePromise
-    .then(function (user) {
-        console.log(user.debugPrinting());
+    .then(function (location) {
+        console.log(location.debugPrinting());
     })
     .catch(function (err) {
-        console.log('Error occurred while calling save');
+        console.log('Error occurred while saving to the database');
+    });
+
+var findPromise3 = mongooseLocation.findMultiple({});
+findPromise3
+    .then(function (locations) {
+        for (var i = 0; i < locations.length; i++) {
+            console.log(locations[i].debugPrinting());
+        }
+    })
+    .catch(function (err) {
+        console.log('No element in the database meets the search criteria');
     });
