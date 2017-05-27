235f385b2af2d379abc6e33d14a69f5eb2271d35
Alberto Spina <as12015@ic.ac.uk>
2017-05-27 11:50:32 +0100

    index.js: Add testing on main file

--- index.js
+++ index.js
@@ -3,13 +3,22 @@ var mongooseEntry = require('./server/mongoose/entry');
 
 var user = mongooseUser.createNewUser('Anne', 'EXAMPLE@example.com', '12345', 'unique 7');
 
-var promise = user.saveUser();
-
-promise
+var savePromise = user.saveUser();
+savePromise
     .then(function (user) {
-    console.log(user.time);
-    console.log('Promise saved with success');
+        console.log(user.debugPrinting());
 })
     .catch(function (err) {
-    console.log('Promise returned an error');
+        console.log('Error occurred while calling save');
 });
+
+var findPromise  = user.findMultiple({name : 'Anne', email: 'example@example.com'});
+findPromise
+    .then(function (users) {
+        for (var i = 0; i < users.length; i++) {
+            console.log(users[i].debugPrinting());
+        }
+    })
+    .catch(function (err) {
+        console.log('No element in the database meets the search criteria');
+    });
