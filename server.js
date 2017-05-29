653b0416f1b41edb62a5542ec83549487636ecb3
Alberto Spina <as12015@ic.ac.uk>
2017-05-29 23:19:50 +0100

    server.js: Add express post handling

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const authentication = require('./app/models/authentication/authentication');
 const routes = require('./app/routes/index');
 const path = require('path');
+const bodyParser = require("body-parser");
 
 let app = express();
 
@@ -10,9 +11,19 @@ authentication.setup(app, (app) => {
   app.use(express.static(path.join(__dirname, '/app/views')));
   app.use(routes);
 
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+
   /* Set the port here */
   app.set('port', (3000));
   app.listen(app.get('port'), () => {
     console.log('started server on port ' + app.get('port'));
   });
+});
+
+app.post('/googlemaps', function(req,res){
+    console.log("Post was caught");
+    let name=req.body.name;
+    console.log("User name = "+name);
+    res.end("yes");
 });
