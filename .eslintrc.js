b6e7dbebb5ce4b912efb1e10b9fd1835b79318b2
Riku Murai <riku.murai15@imperial.ac.uk>
2017-05-29 10:06:37 +0100

    eslintrc.js: Add setup file for eslint

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "extends": "google",
+    "parserOptions": {
+        "ecmaVersion": 6
+    },
+    "rules": {
+        "max-len": [1, 120, 2, { ignoreComments: true }],
+    }
+};
