1f68d73cab451323d99781a5001f3192ef27cfbc
Alberto Spina <as12015@ic.ac.uk>
2017-05-29 14:18:21 +0100

    server: Refactor codebase to follow MEAN stack

--- server/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const authentication = require('../authentication/authentication');
-const router = new express.Router();
-
-router.get('/', authentication.ensureAuthenticated, (req, res)=> {
-    res.render('index');
-});
-
-module.exports = router;
