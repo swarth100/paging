7188a18a584c1b232f8faa66f2c48b4bcdc0e57d
Daniel Boulby <db2515@ic.ac.uk>
2017-05-29 18:33:31 +0100

    app/views/components/app/formController.js: Add data for search fields and function for search

---
+++ app/views/components/app/formController.js
@@ -0,0 +1,9 @@
+angular.module('paging', []).controller('FormController', ['$scope', function($scope) {
+  $scope.location = 'Current Location';
+  $scope.datetime = '';
+  $scope.duration = 1;
+  $scope.radius = 2;
+  $scope.submitFields = () => {
+    alert('Hello');
+  };
+}]);
