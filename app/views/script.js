faadba9b5a53cc4c626c6a9f4f94b1535e827230
Alberto Spina <as12015@ic.ac.uk>
2017-05-29 23:19:06 +0100

    app/views/script.js: Refactor to have navigator.geolocation trigger in controller

--- app/views/script.js
+++ app/views/script.js
@@ -1,5 +1,35 @@
-angular.module('nomeACaso', []).controller('ngAppDemoController', function($scope) {
-    $scope.a = 3;
-    $scope.b = 5;
+angular.module('googleMaps', []).controller('postLocation', function($scope, $http) {
+
+    let url = '/googlemaps';
+
+    console.log("Triggered");
+
+    console.log(navigator);
+    console.log(navigator.geolocation);
+
+    /* DO NOT use firefox browser.
+     * Geolocalisation seems to not be supported :/ */
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+
+            console.log("Navigator?");
+
+            let location = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            console.log("Sad :(");
+
+            $http.post(url, location)
+                .then(function (response) {
+                    console.log("Success?");
+                }, function (response) {
+                    console.log("Failure?");
+                });
+        });
+    } else {
+        console.log("GeoLoc not supported by browser");
+    }
 });
 
