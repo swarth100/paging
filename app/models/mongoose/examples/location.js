c1b15ac0d275684b786bd0076d8be400504515c5
Alberto Spina <as12015@ic.ac.uk>
2017-05-29 14:44:03 +0100

    app/**/**/: Fir -gitignore file excluding directries

---
+++ app/models/mongoose/examples/location.js
@@ -0,0 +1,117 @@
+/* EXAMPLES for mongoose modules */
+
+/* ------------------------------------------------------------------------------------------------------------------ */
+/* location.js -- general*/
+
+/* Require the module */
+let mongooseLocation = require('./server/mongoose/location');
+
+/* Create a new location */
+let location = mongooseLocation.createNewLocation(0, 120, 100, 100);
+
+/* Print the fields of the newly created location */
+console.log(location.id);
+console.log(location.latitude);
+console.log(location.longitude);
+
+/* Apply functions to the newly created location */
+console.log(location.debugPrinting());
+
+/* ------------------------------------------------------------------------------------------------------------------ */
+/* location.js -- mongooseLocation.saveLocation */
+
+/* Save the location to the database */
+let savePromise = mongooseLocation.saveLocation(location);
+savePromise
+    .then(function(location) {
+        console.log(location.debugPrinting());
+    })
+    .catch(function(err) {
+        console.log('Error occurred while saving to the database');
+    });
+
+/* ------------------------------------------------------------------------------------------------------------------ */
+/* location.js -- mongooseLocation.find */
+
+/* Retrieve 1 location from the database */
+let findPromise1 = mongooseLocation.find({id: 0});
+findPromise1
+    .then(function(location) {
+        console.log(location.debugPrinting());
+    })
+    .catch(function(err) {
+        console.log('No element in the database meets the search criteria');
+    });
+
+/* Retrieve 1 location from the database with multiple search criteria */
+let findPromise2 = mongooseLocation.find({latitude: 100, longitude: 100});
+findPromise2
+    .then(function(location) {
+        console.log(location.debugPrinting());
+    })
+    .catch(function(err) {
+        console.log('No element in the database meets the search criteria');
+    });
+
+/* ------------------------------------------------------------------------------------------------------------------ */
+/* location.js -- mongooseLocation.findMultiple */
+
+/* Retrieve multiple locations from the database with multiple search criteria */
+let findPromise3 = mongooseLocation.findMultiple({latitude: 100, longitude: 100});
+findPromise3
+    .then(function(locations) {
+        for (let i = 0; i < locations.length; i++) {
+            console.log(locations[i].debugPrinting());
+        }
+    })
+    .catch(function(err) {
+        console.log('No element in the database meets the search criteria');
+    });
+
+/* Retrieve multiple locations from the database with multiple search criteria. Throws error */
+let findPromise4 = mongooseLocation.findMultiple({latitude: 100, longitude: 100});
+findPromise4
+    .then(function(locations) {
+        for (let i = 0; i < locations.length; i++) {
+            console.log(locations[i].debugPrinting());
+        }
+    })
+    .catch(function(err) {
+        console.log('No element in the database meets the search criteria');
+    });
+
+/* ------------------------------------------------------------------------------------------------------------------ */
+/* location.js -- mongooseLocation.removeLocation */
+
+/* Remove the first entry from the database matching the search criteria */
+let removePromise1 = mongooseLocation.removeLocation({id: 0});
+removePromise1
+    .then(function() {
+        console.log('Location removed');
+    })
+    .catch(function(err) {
+        console.log('No element in the database meets the deletion criteria');
+    });
+
+/* ------------------------------------------------------------------------------------------------------------------ */
+/* location.js -- mongooseLocation.removeMultiple */
+
+/* Removes all entries from the database matching the search criteria */
+let removePromise2 = mongooseLocation.removeMultiple({latitude: 100});
+removePromise2
+    .then(function() {
+        console.log('Locations removed');
+    })
+    .catch(function(err) {
+        console.log('No element in the database meets the deletion criteria');
+    });
+
+/* Clears the database */
+let removePromise3 = mongooseLocation.removeMultiple({});
+removePromise3
+    .then(function() {
+        console.log('Database cleared');
+    })
+    .catch(function(err) {
+        console.log('No element in the database meets the deletion criteria');
+    });
---
+++ app/models/mongoose/location.js
@@ -0,0 +1,144 @@
+// TODO: Add description
+
+/* ------------------------------------------------------------------------------------------------------------------ */
+
+let mongoose = require('mongoose');
+let helper = require('./mongoose');
+
+/* Load the database address from the config file
+ * Removesthe double quotation mark using replace function
+ */
+let config = require('config');
+let dbConfig = JSON.stringify(config.get('dbConfig').host).replace(/\"/g, '');
+
+let uniqueValidator = require('mongoose-unique-validator');
+
+/* Connect to mongoDB location database */
+
+let Schema = mongoose.Schema;
+let locationsDBName = '/locations';
+mongoose.Promise = global.Promise;
+let locationsDB = mongoose.createConnection(dbConfig + locationsDBName);
+
+// TODO: Add validation
+
+/* Handling connection errors */
+locationsDB.on('error', console.error.bind(console, 'Cannot connect to entryDB:'));
+locationsDB.once('open', function() {
+    console.log('Locations DB Active');
+});
+
+let locationsSchema = new Schema({
+    id: {
+        type: Number,
+        unique: true,
+        required: true,
+    },
+    avgtime: {
+        type: Number,
+        required: true,
+    },
+    feedbackcount: {
+        type: Number,
+        required: true,
+        default: 1,
+    },
+    latitude: {
+        type: Number,
+        required: true,
+    },
+    longitude: {
+        type: Number,
+        required: true,
+    },
+});
+
+/* Plugin that validates unique entries */
+locationsSchema.plugin(uniqueValidator);
+
+/* Helper methods on entrySchema */
+locationsSchema.methods.debugPrinting = function() {
+    return 'id: ' + this.id + ', latitude: ' + this.latitude + ', longitude: ' + this.longitude;
+};
+
+/* Pre save function [AUTORUN]
+ * Used to initialise fields upon saving
+ * */
+locationsSchema.pre('save', function(next) {
+    this.time = Date.now();
+
+    // TODO: Handle checks before invoking next
+    // Next can be invoked with an error to make it cascade through
+    // i.e. new Error('something went wrong')
+    next();
+});
+
+/* ------------------------------------------------------------------------------------------------------------------ */
+
+let Location = locationsDB.model('Location', locationsSchema);
+
+/* Creates and returns a new database entry
+ * Parameters:
+ *   a = id
+ *   b = avgtime
+ *   c = latitude
+ *   d = longitude
+ * Returns:
+ *   new Location instance */
+exports.createNewLocation = function(a, b, c, d) {
+    return new Location({
+        id: a,
+        avgtime: b,
+        latitude: c,
+        longitude: d,
+    });
+};
+
+/* Saves the current Location onto the DB
+ * Parameters:
+ *   user
+ * Returns:
+ *   Promise */
+exports.saveLocation = function(loc) {
+    return helper.saveHelper(loc);
+};
+
+/* Retrieves one Location from the DB
+ * Parameters:
+ *   Search parameters : { id : 34 }
+ * Returns:
+ *   Promise */
+exports.find = function(p) {
+    return helper.findHelper(Location, p);
+};
+
+/* Retrieves multiple Locations from the DB
+ * Parameters:
+ *   Search parameters : { id : 34 }
+ * Returns:
+ *   Promise */
+exports.findMultiple = function(p) {
+    return helper.findMultipleHelper(Location, p);
+};
+
+/* Removes a single Location from the DB
+ * Parameters:
+ *   Search parameters : { id : 34 }
+ * Returns:
+ *   Promise */
+exports.removeLocation = function(p) {
+    return helper.removeElem(Location, p);
+};
+
+/* Removes multiple Locations from the DB
+ * Parameters:
+ *   Search parameters : { id : 34 }
+ * Returns:
+ *   Promise */
+exports.removeMultiple = function(p) {
+    return helper.removeMultipleHelper(Location, p);
+};
+
+/* Export the User model */
+exports.locationModel = Location;
+
