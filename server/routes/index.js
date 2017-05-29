9ca5d174369f501d1e95b074ede59f3825cbd7e9
Riku Murai <riku.murai15@imperial.ac.uk>
2017-05-29 10:12:35 +0100

    routes/index.js: Fix code to pass stylecheck

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const authentication = require('../authentication/authentication');
-const router = express.Router();
+const router = new express.Router();
 
 router.get('/', authentication.ensureAuthenticated, (req, res)=> {
     res.render('index');
