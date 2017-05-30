0b1b2ddfa960ef0b8c3c31c645f27d9bc8fde355
Alberto Spina <as12015@ic.ac.uk>
2017-05-30 17:33:30 +0100

    app/views/script.js: Refactor for lint

--- app/views/script.js
+++ app/views/script.js
@@ -9,21 +9,24 @@ app.controller('MainController', function($scope, $filter) {
         $scope.$broadcast('submit');
     };
 
-app.config(function ($routeProvider, $locationProvider) {
+app.config(function($routeProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
 
     $routeProvider
         .when('/',
             {
-                templateUrl: 'components/home/home.html'
+                templateUrl: 'components/home/home.html',
             })
         .when('/login',
             {
-                templateUrl: 'components/login/login.html'
+                templateUrl: 'components/login/login.html',
             })
         .when('/register',
             {
-                templateUrl: 'components/register/register.html'
+                templateUrl: 'components/register/register.html',
             })
-        .otherwise( {redirectTo: '/'});
+        .otherwise( );
+
+    /* TODO: Handle default redirection */
+    // .otherwise( {redirectTo: '/'} );
 });
