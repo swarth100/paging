be71d4bfd5a3b5421f6a741fd00d4fd9d8d72bc8
Alberto Spina <as12015@ic.ac.uk>
2017-05-30 17:16:23 +0100

    app/views/components/home/home.js: Add handling for authentication routing

--- app/views/components/home/home.js
+++ app/views/components/home/home.js
@@ -60,3 +60,16 @@ app.controller('postLocation', function($scope, $http) {
         }
     }
 });
+
+/* Issues a get request to check login credentials.
+ * Accordingly redirects user to app or login screen */
+app.controller('entryRedirect', function($http, $location) {
+    $http.get('/users/index')
+        .then(function(response) {
+            /* Data is packaged into a nasty JSON format.
+             * To access it first one must retrieve the *.data part to distinguish from header */
+            $location.url(response.data.url);
+        }, function(response) {
+            console.log('Failure when accessing /users/index');
+        });
+});
