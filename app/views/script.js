034a60c9476ea375865c24155123a6c73a0f59e7
Daniel Boulby <db2515@ic.ac.uk>
2017-05-30 14:49:12 +0100

    script.js: Add MainController to hold search fields

--- app/views/script.js
+++ app/views/script.js
@@ -1,1 +1,6 @@
-angular.module('paging', []);
+angular.module('paging', []).controller('MainController', function($scope) {
+    $scope.location = 'Current Location';
+    $scope.datetime = '';
+    $scope.duration = 1;
+    $scope.radius = 3;
+});
