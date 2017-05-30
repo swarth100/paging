cba53c97af5e4389fcf2f41286d4e4fcb2593401
Riku Murai <riku.murai15@imperial.ac.uk>
2017-05-30 14:26:19 +0100

    authentication.test.js: Format the code according to style check

--- test/authentication.test.js
+++ test/authentication.test.js
@@ -6,90 +6,90 @@ const expect = chai.expect;
 const authentication = require('../app/models/authentication/authentication');
 
 let checkMsg = (list, param, msg) => {
-  for (let i = 0; i < list.length; i++) {
-    if (list[i].param == param) {
-      return expect(list[i].msg).to.deep.equal(msg);
+    for (let i = 0; i < list.length; i++) {
+        if (list[i].param == param) {
+            return expect(list[i].msg).to.deep.equal(msg);
+        }
     }
-  }
 };
 
 let stubForValidation = (done) => {
-  let req = {
-    query: {},
-    body: {
-      name: '',
-      username: '',
-      email: '',
-      password: '',
-      password2: '',
-    },
-    params: {},
-    param: function(name) {
-      return this.params[name];
-    },
-  };
-  expressValidator(req, {}, function() {
-    done(req);
-  });
+    let req = {
+        query: {},
+        body: {
+            name: '',
+            username: '',
+            email: '',
+            password: '',
+            password2: '',
+        },
+        params: {},
+        param: function(name) {
+            return this.params[name];
+        },
+    };
+    expressValidator(req, {}, function() {
+        done(req);
+    });
 };
 
 describe('Authentication Test', () => {
-  let req = null;
-  let error = null;
-  beforeEach((done) => {
-    stubForValidation((r) => {
-      req = r;
-      authentication.checkRegisterFields(req, (e) => {
-        error = e;
+    let req = null;
+    let error = null;
+    beforeEach((done) => {
+        stubForValidation((r) => {
+            req = r;
+            authentication.checkRegisterFields(req, (e) => {
+                error = e;
+                done();
+            });
+        });
+    });
+    it('Reject empty name field', (done) => {
+        checkMsg(error, 'name', 'name is required');
         done();
-      });
     });
-  });
-  it('Reject empty name field', (done) => {
-    checkMsg(error, 'name', 'name is required');
-    done();
-  });
-  it('Reject empty username fields', (done) => {
-    checkMsg(error, 'username', 'username is required');
-    done();
-  });
-  it('Reject empty email fields', (done) => {
-    checkMsg(error, 'email', 'email is required');
-    done();
-  });
-  it('Reject empty password fields', (done) => {
-    checkMsg(error, 'password', 'password is required');
-    done();
-  });
-  it('Reject invalid email (no @ mark)', (done) => {
-    req.body.email = 'email';
-    authentication.checkRegisterFields(req, (e) => {
-      error = e;
-      done();
+    it('Reject empty username fields', (done) => {
+        checkMsg(error, 'username', 'username is required');
+        done();
     });
-    checkMsg(error, 'email', 'email is invalid');
-    done();
-  });
-  it('Reject password mismatch', (done) => {
-    req.body.password = 'password';
-    req.body.password2 = 'password2';
-    authentication.checkRegisterFields(req, (e) => {
-      error = e;
-      done();
+    it('Reject empty email fields', (done) => {
+        checkMsg(error, 'email', 'email is required');
+        done();
     });
-    checkMsg(error, 'password', 'passwords does not match');
-    done();
-  });
-  it('Accepts valid user detail', (done) => {
-    req.body.name = 'test_name';
-    req.body.username = 'test_username';
-    req.body.email = 'test@test.com';
-    req.body.password = 'password';
-    req.body.password2 = 'password';
-    authentication.checkRegisterFields(req, (e) => {
-      error = e;
-      done();
+    it('Reject empty password fields', (done) => {
+        checkMsg(error, 'password', 'password is required');
+        done();
     });
-    assert.isFalse(error, 'Must allow valid user detail to register');
-  });
+    it('Reject invalid email (no @ mark)', (done) => {
+        req.body.email = 'email';
+        authentication.checkRegisterFields(req, (e) => {
+            error = e;
+            done();
+        });
+        checkMsg(error, 'email', 'email is invalid');
+        done();
+    });
+    it('Reject password mismatch', (done) => {
+        req.body.password = 'password';
+        req.body.password2 = 'password2';
+        authentication.checkRegisterFields(req, (e) => {
+            error = e;
+            done();
+        });
+        checkMsg(error, 'password', 'passwords does not match');
+        done();
+    });
+    it('Accepts valid user detail', (done) => {
+        req.body.name = 'test_name';
+        req.body.username = 'test_username';
+        req.body.email = 'test@test.com';
+        req.body.password = 'password';
+        req.body.password2 = 'password';
+        authentication.checkRegisterFields(req, (e) => {
+            error = e;
+            done();
+        });
+        assert.isFalse(error, 'Must allow valid user detail to register');
+    });
 });
