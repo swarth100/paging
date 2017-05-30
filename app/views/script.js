6a7a39d60695e4b827eb87bb69329bf6635f4afc
Alberto Spina <as12015@ic.ac.uk>
2017-05-30 18:31:38 +0100

    app/views/script.js: Refactor to push controllers to home.js

--- app/views/script.js
+++ app/views/script.js
@@ -1,14 +1,5 @@
 let app = angular.module('paging', []);
 
-app.controller('MainController', function($scope, $filter) {
-    $scope.location = 'Current Location';
-    $scope.datetime = $filter('date')(new Date(), 'yyyy-MM-dd HH:mm');
-    $scope.duration = 60;
-    $scope.radius = 1000;
-    $scope.submitFields = () => {
-        $scope.$broadcast('submit');
-    };
-
 app.config(function($routeProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
 
