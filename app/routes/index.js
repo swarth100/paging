d06db5e7debf976b654dcf10401935b86095c7a5
Alberto Spina <as12015@ic.ac.uk>
2017-05-30 01:11:58 +0100

    app/routes/index.js: Refactor POST handler to be in index.html router

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,26 @@
+/* Routing for index.html file */
+
 const express = require('express');
 const path = require('path');
+const googlemaps = require('./../assets/js/googlemaps');
 const router = new express.Router();
 
-router.get('/', (req, res)=> {
+/* TODO: Look into redirect AND sending file */
+router.get('*', (req, res)=> {
     res.sendFile(path.join(__dirname + '/../views/index.html'));
+});
+
+/* Post handler for /googlemaps */
+router.post('/googlemaps', function(req, res) {
+    console.log('[index.html] : POST request to /googlemaps');
+
+    /* Use above included bodyParser to parse incoming JSON
+     * Parsing is done via the req.body */
+    googlemaps.searchAroundLocation(req.body, function(result) {
+        /* The following line could be needed to specify that JSONs will be sent back via the connection */
+        // res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(result));
+    });
 });
 
 module.exports = router;
