6a226082cce1d134a4d65e1d42531fc68916dfd5
Alberto Spina <as12015@ic.ac.uk>
2017-05-30 01:14:44 +0100

    server.js: Add comments. Refactor router POST hadling to index.js

--- server.js
+++ server.js
@@ -2,28 +2,28 @@ const express = require('express');
 const authentication = require('./app/models/authentication/authentication');
 const routes = require('./app/routes/index');
 const path = require('path');
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 
+/* Defines the application */
 let app = express();
 
-authentication.setup(app, (app) => {
-  /* Set the URI here */
-  app.use(express.static(path.join(__dirname, '/app/views')));
-  app.use(routes);
+/* Body Parser for JSON format variables
+ * Access the variable via the *.body field */
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
-  app.use(bodyParser.urlencoded({ extended: false }));
-  app.use(bodyParser.json());
+/* Set the URI here */
+app.use(express.static(path.join(__dirname, '/app/views')));
+app.use(routes);
 
-  /* Set the port here */
-  app.set('port', (3000));
-  app.listen(app.get('port'), () => {
-    console.log('started server on port ' + app.get('port'));
-  });
+/* Sets the server to port 3000.
+ * Openes port 3000 to listen for connections */
+app.set('port', (3000));
+app.listen(app.get('port'), () => {
+    console.log('[Server] : open on port ' + app.get('port'));
 });
 
-app.post('/googlemaps', function(req,res){
-    console.log("Post was caught");
-    let name=req.body.name;
-    console.log("User name = "+name);
-    res.end("yes");
+/* Basic authentication for users */
+authentication.setup(app, (app) => {
+
 });
