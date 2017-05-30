295190a43d82750ed33b95ff548afa7f8ac4b275
Alberto Spina <as12015@ic.ac.uk>
2017-05-30 17:12:24 +0100

    eslintrc.js: Add ignore rule for trailing empty line

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
     "rules": {
         "max-len": [1, 120, 2, { ignoreComments: true }],
         "require-jsdoc": 0,
-        "no-unused-vars": 0
+        "no-unused-vars": 0,
+        "eol-last": 0
     }
 };
