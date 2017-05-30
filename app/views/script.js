abb1e3f27ee397bf49fdafcdc6854d982fb1a8ca
Alberto Spina <as12015@ic.ac.uk>
2017-05-30 17:38:48 +0100

    app/views/script.js: Add Angular-sided routing for page views

--- app/views/script.js
+++ app/views/script.js
@@ -1,4 +1,6 @@
-angular.module('paging', []).controller('MainController', function($scope, $filter) {
+let app = angular.module('paging', []);
+
+app.controller('MainController', function($scope, $filter) {
     $scope.location = 'Current Location';
     $scope.datetime = $filter('date')(new Date(), 'yyyy-MM-dd HH:mm');
     $scope.duration = 60;
@@ -6,4 +8,22 @@ angular.module('paging', []).controller('MainController', function($scope, $filt
     $scope.submitFields = () => {
         $scope.$broadcast('submit');
     };
+
+app.config(function ($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+
+    $routeProvider
+        .when('/',
+            {
+                templateUrl: 'components/home/home.html'
+            })
+        .when('/login',
+            {
+                templateUrl: 'components/login/login.html'
+            })
+        .when('/register',
+            {
+                templateUrl: 'components/register/register.html'
+            })
+        .otherwise( {redirectTo: '/'});
 });
