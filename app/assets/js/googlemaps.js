25bb38df8ac7592bcb84c95f1443066d3839d8f5
Alberto Spina <as12015@ic.ac.uk>
2017-05-30 01:10:38 +0100

    app/assets/js/googlemaps.js: Add comments for function readability

--- app/assets/js/googlemaps.js
+++ app/assets/js/googlemaps.js
@@ -1,15 +1,20 @@
+/* Given a location JSON and a callback function,
+ * Performs a radar search via the Google API around the given location.
+ * On return calls the callback function */
 function searchAroundLocation(location, cb) {
 
     let googleMapsClient = require('@google/maps').createClient({
         key: 'AIzaSyCAYorWuqzvRAPmNRs8C95Smp7hhdATzc8',
     });
 
+    /* Package a JSON format query for the radar */
     let query = {
         location: location,
         radius: 2000,
         type: 'museum',
     };
 
+    /* Place the radar and return the result to the callback function */
     googleMapsClient.placesRadar(query, function (err, response) {
         if (err) {
             console.log(err);
