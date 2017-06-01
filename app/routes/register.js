4e5b0bdc4adc830d0636ac390a850859b322e4dc
Alberto Spina <as12015@ic.ac.uk>
2017-06-01 15:42:11 +0100

    app/routes/register.js: Refactor routing to reflect validation changes

--- app/routes/register.js
+++ app/routes/register.js
@@ -7,20 +7,7 @@ const router = new express.Router();
 /* Handle post requests on /users/register
  * Explicitally handles form submission for register credentials */
 router.post('/users/register', (req, res) => {
-    passport.checkRegisterFields(req, (error) => {
-        if (error) {
-            /* TODO send the error message down */
-            console.log('[Auth] user creation : failure');
-            console.log(error);
-
-            res.writeHead(401, {'Content-Type': 'application/json'});
-            return res.end();
-        } else {
-            /* TODO send confirmation email */
-            console.log('[Auth] user creation : success');
-            passport.addUser(req, res);
-        }
-    });
+    passport.checkRegisterFields(req, res);
 });
 
 module.exports = router;
