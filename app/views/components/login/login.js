fa6c07aec55e619b12571d9cb92e2ebf98be2a6a
Riku Murai <riku.murai15@imperial.ac.uk>
2017-06-01 12:57:03 +0100

    login.js: Use status code to check valid user login

--- app/views/components/login/login.js
+++ app/views/components/login/login.js
@@ -11,9 +11,9 @@ app.controller('loginFormCtrl', function($scope, $location, $http) {
             .then(function(response) {
                 /* Data is packaged into a nasty JSON format.
                  * To access it first one must retrieve the *.data part to distinguish from header */
-                $location.url(response.data.url);
+                $location.url('/');
             }, function(response) {
-                console.log('Failure when accessing /users/login');
+                $location.url('/login');
             });
     };
 });
