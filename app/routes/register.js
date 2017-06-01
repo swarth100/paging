5a0cc25ceaa36894000871883733fb2d51c83b86
Riku Murai <riku.murai15@imperial.ac.uk>
2017-06-01 11:46:32 +0100

    routes/register.js: Return status code not URL json

--- app/routes/register.js
+++ app/routes/register.js
@@ -7,24 +7,20 @@ const router = new express.Router();
 /* Handle post requests on /users/register
  * Explicitally handles form submission for register credentials */
 router.post('/users/register', (req, res) => {
-    req.checkBody('name', 'name is required').notEmpty();
-    req.checkBody('email', 'email is required').notEmpty();
-    req.checkBody('email', 'email is not valid').isEmail();
-    req.checkBody('username', 'username is required').notEmpty();
-    req.checkBody('password', 'password is required').notEmpty();
-    req.checkBody('password2', 'passwords does not match').equals(req.body.password);
+    passport.checkRegisterFields((error) => {
+        if (error) {
+            /* TODO send the error message down */
+            console.log('[Auth] user creation : failure');
+            console.log(error);
 
-    let error = req.validationErrors();
-    if (error) {
-        /* TODO send the error message down */
-        console.log('[Auth] user creation : failure');
-        console.log(error);
-        res.send(JSON.stringify({'url': '/register'}));
-    } else {
-        /* TODO send confirmation email */
-        console.log('[Auth] user creation : success');
-        passport.addUser(req, res);
-    }
+            res.writeHead(401, {'Content-Type': 'application/json'});
+            return res.end();
+        } else {
+            /* TODO send confirmation email */
+            console.log('[Auth] user creation : success');
+            passport.addUser(req, res);
+        }
+    });
 });
 
 module.exports = router;
