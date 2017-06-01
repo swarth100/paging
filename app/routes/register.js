835b2f188015a60c72f7320d8ac74c06cef63069
Riku Murai <riku.murai15@imperial.ac.uk>
2017-06-01 17:58:43 +0100

    routes/register.js: Use new register account check

--- app/routes/register.js
+++ app/routes/register.js
@@ -7,7 +7,7 @@ const router = new express.Router();
 /* Handle post requests on /users/register
  * Explicitally handles form submission for register credentials */
 router.post('/users/register', (req, res) => {
-    passport.checkRegisterFields(req, res);
+    passport.checkRegisterFields(req, res, passport.addUserToDb, passport.failToValidateUser);
 });
 
 module.exports = router;
