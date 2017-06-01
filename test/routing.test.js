9138a4cab6c6f4d4c0ab26b640a1a098f8bd7545
Riku Murai <riku.murai15@imperial.ac.uk>
2017-06-01 11:47:13 +0100

    routing.test.js: Add test to check routing status code

--- test/routing.test.js
+++ test/routing.test.js
@@ -3,6 +3,7 @@ const chai = require('chai');
 const assert = chai.assert;
 const expect = chai.expect;
 const app = require('../server');
+const userDB = require('../app/models/mongoose/user');
 
 describe('Routing Test', () => {
     it('Root page gives html file with status 200', (done) => {
@@ -10,11 +11,13 @@ describe('Routing Test', () => {
             .expect('Content-Type', /html/)
             .expect(200, done);
     });
+
     it('Error when accessing non-root url', (done) => {
         request(app).get('/hello')
             .expect('Content-Type', /html/)
             .expect(404, done);
     });
+
     it('GET /user/index returns url for home', (done) => {
         request(app).get('/users/index')
             .expect(200)
@@ -22,5 +25,14 @@ describe('Routing Test', () => {
                 expect(response.text).to.deep.equal('{"url":"/home"}');
                 done();
             });
+    });
+
+    it('POST /users/login fails with invalid data', (done) => {
+        request(app).post('/users/login')
+            .send({
+                username: 'HORnRew1jusJSdZPXCLDcqYBTlmm5E',
+                password: 'LAVSzDAzhLiHFVBkxWtWmjjc39AWa44XnVo1JWOU',
+            })
+            .expect(401, done);
     });
 });
