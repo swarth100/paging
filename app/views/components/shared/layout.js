b51253ce9be8e60b430a678bcdea970f541c0e98
Riku Murai <riku.murai15@imperial.ac.uk>
2017-06-02 20:17:33 +0100

    shared/layout.js: Use data from persistant storage

--- app/views/components/shared/layout.js
+++ app/views/components/shared/layout.js
@@ -1,7 +1,21 @@
-app.controller('layoutController', function($scope, $location, $http, LoginData) {
-    $scope.loginData = LoginData;
+app.controller('layoutController', function($scope, $location, $http, $localStorage) {
+    $scope.getEmail = () => {
+        return $localStorage.email;
+    };
+    $scope.getName = () => {
+        return $localStorage.name;
+    };
+    $scope.getUserName = () => {
+        return $localStorage.username;
+    };
+    $scope.isLoggedIn = () => {
+        return $localStorage.isLoggedIn;
+    };
     $scope.logout = function() {
-        $scope.loginData.isLoggedIn = false;
+        $localStorage.isLoggedIn = false;
+        $localStorage.username = '';
+        $localStorage.name = '';
+        $localStorage.email = '';
         $http.get('/users/logout')
             .then(function(response) {
                 $location.url('/');
