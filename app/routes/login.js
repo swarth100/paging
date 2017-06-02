5751cd874a55ea631c75a9e59d7031beb5b42cff
Riku Murai <riku.murai15@imperial.ac.uk>
2017-06-02 15:26:58 +0100

    routes/login.js: Return user object to Angular after login

--- app/routes/login.js
+++ app/routes/login.js
@@ -7,22 +7,22 @@ const passport = require('passport');
 /* Handle post requests on /users/login
  * Explicitally handles form submission for login credentials */
 router.post('/users/login', function(req, res, next) {
-  passport.authenticate('local', function(err, user, info) {
-    if (err) {
-      return next(err);
-    }
-    if (!user) {
-      console.log('no user found');
-      return res.status(401).end();
-    }
-    req.logIn(user, function(err) {
-      if (err) {
-        return next(err);
-      }
-      console.log('user found');
-     return res.status(200).end();
-    });
-  })(req, res, next);
+    passport.authenticate('local', function(err, user, info) {
+        if (err) {
+            return next(err);
+        }
+        if (!user) {
+            console.log('no user found');
+            return res.status(401).end();
+        }
+        req.logIn(user, function(err) {
+            if (err) {
+                return next(err);
+            }
+            console.log('user found');
+            return res.status(200).send(user);
+        });
+    })(req, res, next);
 });
 
 module.exports = router;
