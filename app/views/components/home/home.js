03eec5a7b7e84d876df9427e8d159a231dda448b
Riku Murai <riku.murai15@imperial.ac.uk>
2017-06-02 12:01:59 +0100

    home/home.js: Remove redirect to login

--- app/views/components/home/home.js
+++ app/views/components/home/home.js
@@ -28,27 +28,6 @@ app.controller('homeController', function($scope, $filter, $http, $location, Sea
 
     $scope.submitFields = () => {
         $scope.$broadcast('submit');
-
-        $http.get('/users/search')
-            .then(function(response) {
-                /* Data is packaged into a nasty JSON format.
-                 * To access it first one must retrieve the *.data part to distinguish from header */
-                $location.url(response.data.url);
-            }, function(response) {
-                console.log('Failure when accessing /users/search');
-            });
+        $location.url('/app');
     };
-});
-
-/* Issues a get request to check login credentials.
- * Accordingly redirects user to app or login screen */
-app.controller('homeRedirect', function($http, $location) {
-    $http.get('/users/home')
-        .then(function(response) {
-            /* Data is packaged into a nasty JSON format.
-             * To access it first one must retrieve the *.data part to distinguish from header */
-            $location.url(response.data.url);
-        }, function(response) {
-            console.log('Failure when accessing /users/index');
-        });
 });
