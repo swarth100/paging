e05128a77f6d5a5eeec25d18a25e3aa2ebd69332
Riku Murai <riku.murai15@imperial.ac.uk>
2017-06-02 14:05:00 +0100

    login.js: Set user detail on successful login

--- app/views/components/login/login.js
+++ app/views/components/login/login.js
@@ -2,7 +2,7 @@
 
 /* The following controller is used to submit the loginForm to /users/login
  * The returned address is used to redirect to the correct failure/passing page */
-app.controller('loginFormCtrl', function($scope, $location, $http) {
+app.controller('loginFormCtrl', function($scope, $location, $http, LoginData) {
     /* For default text */
     $scope.master = {};
 
@@ -20,9 +20,12 @@ app.controller('loginFormCtrl', function($scope, $location, $http) {
     $scope.submit = function() {
         $http.post('/users/login', {username: $scope.username, password: $scope.password})
             .then(function(response) {
+                $scope.loginData.isLoggedIn = true;
                 $location.url('/');
             }, function(response) {
                 $scope.alerts[0] = ({msg: 'Invalid username or password'});
             });
     };
+    /* add access to the loginData */
+    $scope.loginData = LoginData;
 });
