79c34941caee44e6ecb363db68398fc05ce12315
Riku Murai <riku.murai15@imperial.ac.uk>
2017-06-02 16:10:25 +0100

    shared/layout.js: Add signout function

--- app/views/components/shared/layout.js
+++ app/views/components/shared/layout.js
@@ -1,4 +1,12 @@
-app.controller('layoutController', function($scope, $filter, LoginData) {
+app.controller('layoutController', function($scope, $location, $http, LoginData) {
     $scope.loginData = LoginData;
-    console.log($scope.loginData);
+    $scope.logout = function() {
+        $scope.loginData.isLoggedIn = false;
+        $http.get('/users/logout')
+            .then(function(response) {
+                $location.url('/');
+            }, function(response) {
+                $location.url('/');
+            });
+    };
 });
