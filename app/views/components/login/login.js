40f61a0d8d82b8a3c32dd700321e1f26c2a7b58a
Riku Murai <riku.murai15@imperial.ac.uk>
2017-06-02 12:02:47 +0100

    login/login.js: Handle routing without get request

--- app/views/components/login/login.js
+++ app/views/components/login/login.js
@@ -20,8 +20,6 @@ app.controller('loginFormCtrl', function($scope, $location, $http) {
     $scope.submit = function() {
         $http.post('/users/login', {username: $scope.username, password: $scope.password})
             .then(function(response) {
-                /* Data is packaged into a nasty JSON format.
-                 * To access it first one must retrieve the *.data part to distinguish from header */
                 $location.url('/');
             }, function(response) {
                 $scope.alerts[0] = ({msg: 'Invalid username or password'});
