111baa746aec67edca2311518211e6e922a773f8
Alberto Spina <as12015@ic.ac.uk>
2017-06-05 13:24:02 +0100

    app/views/script.js: Add routing for newly intoduced app/rooms

--- app/views/script.js
+++ app/views/script.js
@@ -9,29 +9,40 @@ app.config(function($routeProvider, $locationProvider) {
     $routeProvider
         .when('/',
         {
-            templateUrl: 'components/index/index.html',
+            templateUrl: '/components/index/index.html',
         })
         .when('/home',
         {
-            templateUrl: 'components/home/home.html',
+            templateUrl: '/components/home/home.html',
         })
-        .when('/app',
+        .when('/app/:foo',
         {
-            templateUrl: 'components/app/app.html',
+            templateUrl: '/components/app/app.html',
         })
         .when('/login',
         {
-            templateUrl: 'components/login/login.html',
+            templateUrl: '/components/login/login.html',
         })
         .when('/register',
         {
-            templateUrl: 'components/register/register.html',
+            templateUrl: '/components/register/register.html',
         })
         .otherwise(
         {
-            templateUrl: 'components/error/error.html',
+            templateUrl: '/components/error/error.html',
         });
 
     /* TODO: Handle default redirection */
     // .otherwise( {redirectTo: '/'} );
+});
+
+/* Default socket connection/initialisation */
+app.controller('indexCtrl', function($scope, socket) {
+    socket.on('connect', (data) => {
+        socket.join('hello-world');
+        socket.broadcast('hello-world', 'messages', 'broadcast');
+    });
+    socket.on('messages', function(data) {
+        console.log('Incoming message:', data);
+    });
 });
