44d25297b23e624251e734e07ea7bff1cedf7e6f
Daniel Boulby <db2515@ic.ac.uk>
2017-06-05 12:04:43 +0100

    home.js: Fix confilcts between sessionStorage and types code

--- app/views/components/home/home.js
+++ app/views/components/home/home.js
@@ -2,18 +2,6 @@
 
 /* Controller to handle the search bar on the home screen */
 app.controller('homeController', function($scope, $filter, $http, $location, $sessionStorage, socket) {
-    $scope.homeSearch = Search;
-
-    $scope.tmpDate = new Date();
-    $scope.tmpTime = new Date();
-
-    /* Initialises the following fields to the following default values */
-    $scope.homeSearch.location = 'Current Location';
-    $scope.homeSearch.datetime = '';
-    $scope.homeSearch.duration = 60;
-    $scope.homeSearch.radius = 1000;
-    $scope.homeSearch.type = [];
-
     socket.on('connect', (data) => {
         socket.join('hello-world');
         socket.broadcast('hello-world', 'messages', 'broadcast');
@@ -42,7 +30,7 @@ app.controller('homeController', function($scope, $filter, $http, $location, $se
             datetime: '',
             duration: 60,
             radius: 1000,
-            type: 'cafe',
+            type: [],
         };
         $scope.setDate();
         $sessionStorage.queryData = $scope.homeSearch;
