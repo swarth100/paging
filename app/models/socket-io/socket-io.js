5d33d7f67f9f9166ce79d5131dd726da1cb63bd6
Alberto Spina <as12015@ic.ac.uk>
2017-06-05 13:25:04 +0100

    app/models/socket-io/socket-io.js: Add comments

--- app/models/socket-io/socket-io.js
+++ app/models/socket-io/socket-io.js
@@ -1,7 +1,11 @@
 exports.start = (server) => {
+    /* Starts socket.io to be listening on the specific server */
     let io = require('socket.io').listen(server);
+
+    /* Listens for 'connection' messages
+     * 'connection' messages are issues by front-end socket-io.js via the io.connect() command */
     io.on('connection', function(socket) {
-       socket.on('join', (data) => {
+        socket.on('join', (data) => {
             socket.join(data);
             socket.emit('messages', 'thank you for joining');
         });
