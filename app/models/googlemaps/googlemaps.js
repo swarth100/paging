ce337f54da1abf36def27a124a51838516efdede
Riku Murai <riku.murai15@imperial.ac.uk>
2017-06-06 17:31:34 +0100

    googlemaps/googlemaps.js: Add new API key

--- app/models/googlemaps/googlemaps.js
+++ app/models/googlemaps/googlemaps.js
@@ -4,7 +4,8 @@ let avgTimes = require('./average-times');
 let googleMapsClient = require('@google/maps').createClient({
     // key: 'AIzaSyCAYorWuqzvRAPmNRs8C95Smp7hhdATzc8',
     // key: 'AIzaSyD_UOu_gSsRAFFSmEEKmR7fZqgDmvmMJIg',
-    key: 'AIzaSyDZfSnQBIu3V5N9GWbpKGtAUYmDDyxPonU',
+    // key: 'AIzaSyDZfSnQBIu3V5N9GWbpKGtAUYmDDyxPonU',
+    key: 'AIzaSyD7c_7yNAAQc6mhE_JremnfrnUyxvFvfz4',
     Promise: Promise,
 });
 
