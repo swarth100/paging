be3d731bd4fc791c4c3628c3ca0d2dbbc88ff7cd
Alberto Spina <as12015@ic.ac.uk>
2017-06-17 20:22:18 +0100

    app/views/components/app/app.js: Add googlemap hook on closing event

--- app/views/components/app/app.js
+++ app/views/components/app/app.js
@@ -60,6 +60,7 @@ app.controller('appCtrl', function($scope, $http, $routeParams, $filter, $uibMod
     let markers = [];
     let mapObjects = [];
     let users;
+    let map;
     let lastOpenedInfoBubble = undefined;
 
     /*
@@ -564,7 +565,7 @@ app.controller('appCtrl', function($scope, $http, $routeParams, $filter, $uibMod
     /* Functions called upon entry */
 
     $scope.joinRoom();
-    let map;
+
     $scope.getLocation(function(currLoc) {
         map = createMap(currLoc);
         directionsDisplay.setMap(map);
@@ -1253,6 +1254,41 @@ app.controller('appCtrl', function($scope, $http, $routeParams, $filter, $uibMod
     /* Handles toggleing of leftNav */
     $scope.toggleLeftNav = function() {
         console.log('left click');
+
+        /** **********************************************/
+
+        /* Kind gift of:
+         * https://stackoverflow.com/questions/3437786/get-the-size-of-the-screen-current-web-page-and-browser-window
+         *  */
+
+        if (map) {
+            // latlng is the apparent centre-point
+            // offsetx is the distance you want that point to move to the right, in pixels
+            // offsety is the distance you want that point to move upwards, in pixels
+            // offset can be negative
+            // offsetx and offsety are both optional
+
+            let latlng = map.getCenter();
+
+            let offsetx = $(window).width()/4;
+            let offsety = 0;
+
+            let scale = Math.pow(2, map.getZoom());
+
+            let worldCoordinateCenter = map.getProjection().fromLatLngToPoint(latlng);
+            let pixelOffset = new google.maps.Point((offsetx / scale) || 0, (offsety / scale) || 0);
+
+            let worldCoordinateNewCenter = new google.maps.Point(
+                worldCoordinateCenter.x - pixelOffset.x,
+                worldCoordinateCenter.y + pixelOffset.y
+            );
+
+            let newCenter = map.getProjection().fromPointToLatLng(worldCoordinateNewCenter);
+
+            map.setCenter(newCenter);
+        }
+
+        /** **********************************************/
 
         /* */
         $scope.sideLeftBarAnimating = true;
