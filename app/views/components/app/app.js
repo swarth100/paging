9165e26dc1ddde4fc84a4ce155058eebc6da0d1f
Riku Murai <riku.murai15@imperial.ac.uk>
2017-06-19 00:10:52 +0100

    app/app.js: Only set necessary fields to 0

--- app/views/components/app/app.js
+++ app/views/components/app/app.js
@@ -1244,23 +1244,29 @@ app.controller('appCtrl', function($scope, $http, $routeParams, $filter, $uibMod
 
     /* Allow only one type at a type */
     $scope.setAccordionOptions = (type) => {
-        $('#left-nav-options > .option > .panel-body').css('max-height', '0px');
-        $('#left-nav-users > .user > .panel-body').css('max-height', '0px');
-        $('#left-nav-credits > .credit > .panel-body').css('max-height', '0px');
-
         if (type === 'options') {
             let height = document.getElementById('options-body').scrollHeight;
             $('#left-nav-options > .option > .panel-body').css('max-height', height.toString() + 'px');
+
+            $('#left-nav-users > .user > .panel-body').css('max-height', '0px');
+            $('#left-nav-credits > .credit > .panel-body').css('max-height', '0px');
         } else if(type === 'users') {
             let height = document.getElementById('users-body').scrollHeight;
             $('#left-nav-users > .user > .panel-body').css('max-height', height.toString() + 'px');
+
+            $('#left-nav-options > .option > .panel-body').css('max-height', '0px');
+            $('#left-nav-credits > .credit > .panel-body').css('max-height', '0px');
         } else if(type === 'credit') {
             let height = document.getElementById('credits-body').scrollHeight;
             $('#left-nav-credits > .credit > .panel-body').css('max-height', height.toString() + 'px');
+
+            $('#left-nav-options > .option > .panel-body').css('max-height', '0px');
+            $('#left-nav-users > .user > .panel-body').css('max-height', '0px');
         } else {
             console.log('accordion type mismatch');
         }
     };
+
 
     /* ---------------------------------------------------------------------------------------------------------------*/
     /* Bootstrap helper functions */
