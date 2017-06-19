d1445cb4e89eee4d51ca9262fdd61089f43e6c5d
Daniel Boulby <db2515@ic.ac.uk>
2017-06-19 12:22:07 +0100

    socket-io.js: Move place details to be got on search not click

--- app/models/socket-io/socket-io.js
+++ app/models/socket-io/socket-io.js
@@ -253,12 +253,6 @@ exports.start = (server) => {
                 console.log(err);
             }
         });
-
-        socket.on('getPlaceDetails', (location) => {
-            googlemaps.getPlaceDetails(location, function(res) {
-                socket.emit('receivePlaceDetails', res.json.result);
-            });
-        });
     });
 
 
